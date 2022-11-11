279b98ee95b4f052a7f2af108d0d75f859faf91e
22357 <2235715529@qq.com>
2022-11-11 20:19:42 +0800

    20221111

---
+++ python_data_analysitic/csv_demo/pandas_read_csv.py
@@ -0,0 +1,9 @@
+import sys
+import pandas as pd
+
+input_file = '../supplier_data.csv'
+output_file = 'supplier_data_test.csv'
+data_frame = pd.read_csv(input_file)
+print(data_frame)
+data_frame.to_csv(output_file, index=False)  # 可选mode='a'进行追加
+
